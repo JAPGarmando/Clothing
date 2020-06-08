9e5f52ad43d0449eb6994d7e9e4684199836e309
JAPGarmando <jarmando_gama@hotmail.com>
2020-06-08 00:59:03 -0500

    collection preview and items component created

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = ()=>(
-  <div>
-   <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} /> 
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
+      
       </Switch>
     </div>
   );
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,7 +11,7 @@ class Directory extends React.Component{
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'hats',
+                  linkUrl: 'shop/hats',
 
                 },
                 {
